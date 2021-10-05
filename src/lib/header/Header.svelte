c1182a2a3480872c5266a9a2b090f154ca106213
Luca <fulgenzi.luca@gmail.com>
2021-10-05 17:40:17 +0200

    Add header and sidebar

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,120 +1,54 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+<script>
+	import Sidebar from '$lib/header/Sidebar.svelte';
+	let sidebar_show = false;
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
+	<div class='corner pointer'>
+		<img src='./static/settings.png' width='32px' height='auto' alt='setting' id='setting' on:click={ () => sidebar_show = !sidebar_show}>
+	</div>
+	<div>
+		<p id='title'>JA Dashboard</p>
+	</div>
+	<div class='corner pointer'>
+		<img src='./static/github.png' width='32px' height='auto' alt='github'>
 	</div>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
+	<Sidebar bind:show={sidebar_show} />
 </header>
 
 <style>
 	header {
+		width: 100%;
+    background: #fafafa;
+		box-shadow: 0 0.75rem 0.8rem rgba(0, 0, 0, 0.15);
 		display: flex;
 		justify-content: space-between;
+		align-content: center;
+	}
+
+	#title {
+		font-size: 1.3rem;
+		font-weight: 300;
+		color: var(--text-color);
 	}
 
 	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
 		display: flex;
 		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
+		padding: 0 2rem;
 	}
 
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
+	#setting:hover {
+    animation:spin 4s linear infinite;
 	}
 
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
+  @keyframes spin {
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform:rotate(360deg);
+    }
+  }
 
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
 
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--accent-color);
-	}
 </style>
---
+++ src/lib/header/Sidebar.svelte
@@ -0,0 +1,79 @@
+<script>
+	import { fly } from 'svelte/transition';
+	export let show = false;
+</script>
+
+{#if show}
+	<nav transition:fly={{x: -250, opacity: 1}}>
+		<div class='side-header'>
+			<p class='side-header-text'>Setting</p>
+			<img class='pointer' src='./static/cancel.png' alt='r' width='16px' height='16px' on:click={() => show = !show}>
+		</div>
+		<div>
+			<ul class='list'>
+				<li class='list-item pointer'>
+					Primo
+				</li>
+				<li class='list-item pointer'>
+					Secondo
+				</li>
+				<li class='list-item pointer'>
+					Terzo
+				</li>
+			</ul>
+
+		</div>
+
+	</nav>
+{/if}
+
+<style>
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    padding: 2rem 0 0.6rem;
+    border-right: 1px solid #e0e0e0;
+    background: #fff;
+    overflow-y: auto;
+    width: 18rem;
+  }
+
+	.side-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 1.4rem 0;
+	}
+
+	.side-header-text {
+    font-weight: 300;
+	}
+
+	.list {
+		margin: 0 auto;
+		padding: 0;
+    width: 100%;
+		text-decoration: none;
+		list-style: none;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+	}
+
+	.list-item {
+		width: 100%;
+		background-color: #f1f1f1;
+		font-size: 1.2rem;
+		font-weight: 300;
+		padding: 1rem;
+		margin: 1px;
+	}
+
+	.list-item:hover {
+    background-color: #d9d9d9;
+		transition: background-color .5s;
+	}
+</style>
